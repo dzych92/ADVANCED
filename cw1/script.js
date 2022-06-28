a4e7383eb1661eaa68783aa21f30061d195b743b
Dawid <dawidzych92@gmail.com>
2022-06-28 19:44:35 +0200

    ff

--- cw1/script.js
+++ cw1/script.js
@@ -1,22 +1,25 @@
 const data = [
-  {
-    title : "Pink lake",
-    src: "1.webp",
-  },
-  {
-    title : "Single Island",
-    src: "2.webp",
-  },
-  {
-    title : "Forest lake",
-    src: "3.webp",
-  },
-]
-
-function changeHTMLInfo( slideNr ){
-  const title = data[slideNr].title
-  const titleDOM = document.querySelector("h2")
-  titleDOM.innerHTML = title
-}
-
-changeHTMLInfo(1)
+    {
+      title : "Pink lake",
+      src: "1.webp",
+    },
+    {
+      title : "Single Island",
+      src: "2.webp",
+    },
+    {
+      title : "Forest lake",
+      src: "3.webp",
+    },
+  ]
+  
+  function changeHTMLInfo( slideNr ){
+    const title = data[slideNr].title
+    const titleDOM = document.querySelector("h2")
+    titleDOM.innerHTML = title
+    const image = data[slideNr].src
+    const imageDOM = document.querySelector("img")
+    imageDOM.src = "img/"+image
+  }
+  
+  changeHTMLInfo(0)
